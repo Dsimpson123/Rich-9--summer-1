18067aafad5484cf485afc26c56ea2847885cee1
Darren Simpson <darren.simpson@hg.hiddengenius.org>
2025-07-09 13:35:39 -0700

    TravelBagComplete

---
+++ Python/week 1/Week 4/TravelBag.py
@@ -0,0 +1,30 @@
+Bedroom =["Underwear", "Shoes", "Shirts", "Shorts", "Pajamas", "Pants"]
+
+travelBag = []
+print("Pack your bags")
+print("Enter the index of an item you'd like to move from your room to your bag")
+print("Type 'done' when are done packing")
+for item in Bedroom:
+    print(item)
+
+item = int(input("Item index"))
+
+while item != 100:
+ travelBag.append(Bedroom(item))
+item = input("Remove item from Bedroom:")
+Bedroom.remove(Bedroom[item])
+print(Bedroom)
+print("/nYour Travel Bag")
+print(travelBag)
+item = int(input("Item index: "))
+
+print("\nYou travel bag:")
+for item in travelBag:
+   print(item)
+
+
+
+
+
+
+
