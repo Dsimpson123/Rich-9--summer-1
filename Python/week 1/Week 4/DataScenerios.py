96b60e62f4648ba06a845a7aaf5eaa1e510016f4
Darren Simpson <darren.simpson@hg.hiddengenius.org>
2025-07-10 14:33:39 -0700

    DataSceneriosComplete

---
+++ Python/week 1/Week 4/DataScenerios.py
@@ -0,0 +1,80 @@
+
+1. #A restaurant menu with prices for each item
+2. #High scores to an arcade game
+3. #All of the months of the year
+4. #All the items in your backpack
+5. #Look up Student emails by their names
+6. #A shopping cart of groceries 
+print("Scenrio #1:A restaurant menu with prices for each item")
+print("Best Structure: best to pair food with price in key/value")
+menu ={
+    "French Toast": 12,
+    "Grand Slam": 12,
+    "T-Bone Steak": 18,
+    "Avocado Toast": 15,
+
+
+}
+for key, item in menu.item():
+    print(Key, ": ", item)
+
+
+print("Scenario #2: High scores to an arcade game")
+print("Best Structure list: Need a mutable structure to update real time")
+highScores = [
+    100,
+    105,
+    110,
+    99
+
+]
+for score in highScores:
+    print(score)
+
+print(" Scenario #3 #All of the months of the year")
+print("Best structure list: Tuple: We need a collection that does not change ")
+monthsInYear = {
+
+    "January",
+    "Febuary",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "Setember",
+    "October",
+    "Novemeber",
+    "December",
+}
+for month in monthsInYear:
+    print(month)
+
+
+print("Scenerio #4  #All the items in your backpack ")
+print("Best structure: List: We need items that you can change and switch up")
+items ={
+    "Books"
+    "Pencils"
+    "Folders"
+    "Notebooks"
+    "Pens"
+
+
+
+}
+for item in backpack:
+    print(item)
+
+print("Scenrio #5 Look up Student emails by their names" )
+print("Best structure:Dictionary ")
+
+studentEmails ={
+
+
+
+
+
+
+}
