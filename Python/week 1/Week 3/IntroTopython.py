ed3455bfc59e9d20881e9096793545d786443766
Darren Simpson <darren.simpson@hg.hiddengenius.org>
2025-07-29 11:29:56 -0700

    IntroToPythonComplete

---
+++ Python/week 1/Week 3/IntroTopython.py
@@ -0,0 +1,8 @@
+geniuses = ["Kristopher", "Darren"]
+for genius in geniuses:
+    print(genius)
+answer = input("Do you want me to print the list again? Y/N")
+while answer == "Y":
+    for genius in geniuses:
+        print(geniuses)
+    answer = input("Do you want me to print the list again? Y/N")
